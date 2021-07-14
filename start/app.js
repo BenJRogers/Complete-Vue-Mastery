a4345c0dc0df401989cc5c893b38d7ec69fe38e3
Ben Rogers <benrogers@Bens-MBP.modem>
2021-07-14 19:16:41 +1000

    learnt about handling events

--- start/app.js
+++ start/app.js
@@ -3,12 +3,20 @@ const vm = Vue.createApp({
         return {
             firstName: "Ben",
             lastName: "Rogers",
-            url: "https://google.com"
+            url: "https://google.com",
+            rawUrl: '<a href="https://google.com" target="_blank">Google</a>',
+            age: 20
         }
     },
     methods: {
         getFullName() {
             return `${this.firstName} ${this.lastName}`;
+        },
+        changeAge(addOrSubtract) {
+            addOrSubtract ? this.age++ : this.age--;
+        },
+        updateLastName(event) {
+            this.lastName = event.target.value;
         }
     }
 }).mount('#app');
