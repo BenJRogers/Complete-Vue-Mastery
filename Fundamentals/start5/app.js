f6bdeb740fe22d1f3a6618df4ffb065473f95b89
Benjamin Rogers <benjrogers48@gmail.com>
2021-07-15 15:36:05 +1000

    Learn about Vue lifecycle Hooks

--- Fundamentals/start5/app.js
+++ Fundamentals/start5/app.js
@@ -1,8 +1,32 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
+      message: " - Vue lifecycle Hooks!"
     }
+  },
+  beforeCreate() {
+    console.log("beforeCreate() function called", this.message);
+  },
+  created() {
+    console.log("created() function called", this.message);
+  },
+  beforeMount() {
+    console.log("beforeMount() function called", this.$el);
+  },
+  mounted() {
+    console.log("mounted() function called", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdated() function called");
+  },
+  updated() {
+    console.log("updated() function called");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount() function called");
+  },
+  unmounted() {
+    console.log("unmounted() function called");
   }
 })
 
