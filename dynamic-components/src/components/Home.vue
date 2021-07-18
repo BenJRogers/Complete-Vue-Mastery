e85725dc71fccca6278322f943a6fb76e8896dc4
Benjamin Rogers <benjrogers48@gmail.com>
2021-07-19 01:35:35 +1000

    learnt about dynamic components

---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("component was unmounted");
+  },
+
+  deactivated() {
+    console.log("component was deactivated");
+  },
+};
+</script>
