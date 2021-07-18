e79b12101e5f8040dac59ab23daf5989dc39a472
Benjamin Rogers <benjrogers48@gmail.com>
2021-07-18 10:42:39 +1000

    start of single file vue

---
+++ components/src/Components/Greeting.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+<script>
+export default {
+  name: "Greeting",
+  data() {
+    return {
+      msg: "this is a greeting",
+    };
+  },
+};
+</script>
+<style scoped>
+p {
+  color: rgb(1, 153, 1);
+}
+</style>
