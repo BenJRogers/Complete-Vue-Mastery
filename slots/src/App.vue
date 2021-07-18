85418a651ed0160b94671d2454ea6d5ee93b8174
Benjamin Rogers <benjrogers48@gmail.com>
2021-07-19 01:19:45 +1000

    learnt about slots

---
+++ slots/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <AppForm>
+    <template v-slot:help>
+      <p>{{ helpText }}</p>
+    </template>
+    <template class="fields" v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+    </template>
+    <template class="buttons" v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+    <p>This should go in the default slot</p>
+  </AppForm>
+  <AppForm>
+    <template v-slot:help>
+      <p>This is a Contact help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </AppForm>
+</template>
+
+<script>
+import AppForm from "./components/AppForm.vue";
+export default {
+  name: "App",
+  components: {
+    AppForm,
+  },
+  data() {
+    return {
+      helpText: "This is some help text",
+    };
+  },
+};
+</script>
+
