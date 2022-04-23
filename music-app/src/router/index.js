aafd389447e86e2dbfc19d67cc83d41472978872
Benjamin Rogers <benjrogers48@gmail.com>
2022-04-23 18:43:20 +1000

    setup base project with dependencies

---
+++ music-app/src/router/index.js
@@ -0,0 +1,8 @@
+// import { createRouter, createWebHistory } from "vue-router";
+// import store from "../store";
+
+// const routes = [];
+
+// const router = createRouter({});
+
+// export default router;
